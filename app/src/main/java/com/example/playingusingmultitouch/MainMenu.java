a5195c152d32d9d2f3773078e9ea6d2bc72b3838
zerosfinal <zerosfinal@gmail.com>
2020-01-24 09:31:12 +0100

    1 versión solo con MainActivity y MainMenu

---
+++ app/src/main/java/com/example/playingusingmultitouch/MainMenu.java
@@ -0,0 +1,31 @@
+package com.example.playingusingmultitouch;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenu extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.home) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
